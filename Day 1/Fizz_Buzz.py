86572a6d7167cf6d84d5e3373508f4676aa07fea
Muhammad Sami Ullah <muhammadsamiyt5@gmail.com>
2026-08-28 12:02:56 +0500

    Fizz_Buzz problem

---
+++ Day 1/Fizz_Buzz.py
@@ -0,0 +1,13 @@
+def fizz_buzz(input):
+    if(input%3==0 and input%5==0):
+        return "FizzBuzz"
+    elif(input%3==0):
+        return "Fizz"
+    elif(input%5==0):
+        return "Buzz"
+    
+    else:
+        return input
+
+input=int(input("Enter your input: "))
+print(fizz_buzz(input))
