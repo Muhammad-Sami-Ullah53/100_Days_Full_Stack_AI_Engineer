4980c3f3db168b5925fa0de5dc39ce66dd4954ea
Muhammad Sami Ullah <muhammadsamiyt5@gmail.com>
2026-08-29 10:42:52 +0500

    Adding Dictionaries

---
+++ Day 2/Dictionaries.py
@@ -0,0 +1,12 @@
+# Dicitonaries me value ko store krny k lye us ka koi naam dena prrta he jisy key kahty hein
+# is me key aur value dono ka use hota he
+# key mtlb 1 naam orr value mtlb us key ki value
+# Jb dictionayry bnani ho to {} ka use krty hn
+# Jb dictionary ki koi value access krni ho to [] ka use krty hn orr us me key ka naam likhty hn
+
+Student = {
+    "name": "Sami",
+    "rollno": 1,
+    "dept": "Computer Science"
+}
+print(Student["name"])
