4ec23e242759e46cf6314eff64f2c10f4cdb3064
Muhammad Sami Ullah <muhammadsamiyt5@gmail.com>
2026-09-06 16:00:01 +0500

    Removing Day 10

--- Day 10/Project_1/Ticket_Support_System/app/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Ticket:
-    ticket_id: int
-    customer: str
-    email: str
-    message: str
--- Day 10/Project_1/Ticket_Support_System/app/storage.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def load_tickets(file_path):
-    with open(file_path, "r") as file:
-        return json.load(file)
